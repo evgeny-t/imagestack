f21c475559ef9065b45a07647d6d7ee9b56ffbe2
andrew.b.adams <andrew.b.adams@f3e031b0-0bc6-11df-b730-f17d7b42d8b9>
2012-06-25 21:22:49 +0000

    Added test for assemblehdr

--- branches/newimage/src/HDR.h
+++ branches/newimage/src/HDR.h
@@ -5,9 +5,10 @@
 class AssembleHDR : public Operation {
 public:
     void help();
+    bool test();
     void parse(vector<string> args);
     static Image apply(Image frames);
-    static Image apply(Image frames, vector<float> &exposures, string gamma="1.0");
+    static Image apply(Image frames, const vector<float> &exposures, string gamma="1.0");
 private:
     enum CutoffType { Regular, LongestExposure, ShortestExposure};
     static float weightFunc(float maxVal, CutoffType c = Regular);
